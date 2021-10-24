d9b8fbd56cc0ddb9571b17a8e8d64f44a7e398ef
grishanych <alsolo2011@gmail.com>
2021-10-24 23:46:31 +0300

    done lessons from 83 to 92

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,18 @@
+import App from "../app/app";
+
+import './app-info.css';
+
+
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+                <h1>Число співробітників в компанії N</h1>
+                <h2>Загальна кількість співробітників: </h2>
+                <h2>Премію отримають: </h2>
+
+        </div>
+    )
+}
+
+export default AppInfo;
